5857d5814b88f5eea4fd3111a9a6a43369093a87
ajewe <ajewe@utexas.edu>
2020-07-02 14:05:29 -0500

    inputs are created when clicking in view entry component

--- src/components/ViewEntry.js
+++ src/components/ViewEntry.js
@@ -1,17 +1,50 @@
 import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import { TextField, Button } from '@material-ui/core';
 /* global ChemDoodle */
 
 export const ViewEntry = (props) => {
   const selectedLogId = props.match.params.id
   const [ sketcher, setSketcher ] = React.useState(null);
 
-  const findSelectedLog = state => {
+    const findSelectedLog = state => {
     return state.logs.find(l => l.logId == selectedLogId)
   }
   const selectedLog = useSelector(state => findSelectedLog(state))
 
+  const [ editEntry, setEditEntry ] = React.useState({
+    changesMade: false,
+    quickInfoShowInput: false,
+    procedureShowInput: false,
+    resultsShowInput: false,
+    yieldShowInput: false,
+    procedures: selectedLog.procedures.map((_, i) => ({
+      dateShowInput: false,
+      entryShowInput: false,
+    }))
+  })
+
+  // const findSelectedLog = state => {
+  //   return state.logs.find(l => l.logId == selectedLogId)
+  // }
+  // const selectedLog = useSelector(state => findSelectedLog(state))
+
+  // const enterDateAndEntries = () => {
+  //   let newEditEntryObject = editEntry
+
+  //   selectedLog.procedures.map((_, i) => {
+  //     newEditEntryObject.procedures.push(
+  //       {
+  //         dateShowInput: false,
+  //         entryShowInput: false,
+  //       }
+  //     )
+  //     setEditEntry(newEditEntryObject)
+  //   })
+  // }
+
   useEffect(() => {
+    // enterDateAndEntries()
     //make sketcher responsive***
     let myCanvas = new ChemDoodle.SketcherCanvas("canvas-id", "600", "350", {
       useServices: false,
@@ -28,26 +61,121 @@ export const ViewEntry = (props) => {
     }
     setSketcher(myCanvas)
   }, [])
-  //need a 'save changes' button to appear somehow with changes to canvas
-  
+
+  //'save changes' or 'discard changes' buttons appear when clicking on canvas
+  let canvasClicked = () => {
+    let newEditEntry = { ...editEntry, changesMade: true }
+    setEditEntry(newEditEntry)
+  }
+
   return (
     <div id="view-entry-paper">
       <div id="view-entry-pattern">
         <div id="view-entry-content">
-          <h1>{selectedLog.bookName}: Entry {selectedLog.bookEntryNumber} </h1>
-          <h2>Quick Info: {selectedLog.quickInfo} </h2>
-          <canvas id="canvas-id" />
-          <h2>Procedure: </h2>
-            {selectedLog.procedures.map(p => {
-              return (
-                <div className="view-entry-procedure-div">
-                  <h2 className="view-entry-procedure-date">{p.date} </h2>
-                  <h2 className="view-entry-procedure-entry">{p.entry}</h2>
-                </div>
-              )
-            })}
-          <h2>Results: {selectedLog.results} </h2>
-          <h2>Yield: {selectedLog.yield} </h2>
+          <h1>{selectedLog.bookName}: Entry {selectedLog.bookEntryNumber} </h1 >
+          <form>
+            <div onClick={() => {
+              setEditEntry({
+                ...editEntry, 
+                quickInfoShowInput: true, 
+                changesMade: true,
+              })
+            }}>
+              <h2 className="view-entry-text">Quick Info: </h2>
+              {editEntry.quickInfoShowInput ? 
+                <input value={selectedLog.quickInfo} ></input>
+                :
+                <h2 className="view-entry-text">{selectedLog.quickInfo} </h2>
+              }
+            </div>
+            <canvas id="canvas-id" onMouseDown={() => canvasClicked()} />
+            <br />
+            <h2 className="view-entry-text">Procedure: </h2>
+              {selectedLog.procedures.map((p, i) => {
+                return (
+                  <div className="view-entry-procedure-div">
+                    {editEntry.procedures[i].dateShowInput ?
+                      <input value={p.date}></input>
+                    :
+                      <h2 className="view-entry-procedure-date" 
+                          onClick={() => {
+                            setEditEntry({
+                              ...editEntry,
+                              changesMade: true,
+                              procedures: [
+                                ...editEntry.procedures.slice(0, i),
+                                {
+                                  ...editEntry.procedures[i],
+                                  dateShowInput: true,
+                                },
+                                ...editEntry.procedures.slice(i + 1)
+                              ]
+                            })
+                          }}
+                      >
+                        {p.date} 
+                      </h2>
+                    }
+                    {editEntry.procedures[i].entryShowInput ?
+                      <input value={p.entry}></input>
+                    :
+                      <h2 className="view-entry-procedure-entry"
+                          onClick={() => {
+                            setEditEntry({
+                              ...editEntry,
+                              changesMade: true,
+                              procedures: [
+                                ...editEntry.procedures.slice(0, i),
+                                {
+                                  ...editEntry.procedures[i],
+                                  entryShowInput: true,
+                                },
+                                ...editEntry.procedures.slice(i + 1)
+                              ]
+                            })
+                          }}
+                      >
+                        {p.entry}
+                      </h2>
+                    }
+                  </div>
+                )
+              })}
+            <div onClick={() => {
+              setEditEntry({
+                ...editEntry, 
+                resultsShowInput: true, 
+                changesMade: true,
+              })
+            }}>
+              <h2 className="view-entry-text">Results: </h2>
+              {editEntry.resultsShowInput ?
+                <input value={selectedLog.results}></input>
+                :
+                <h2 className="view-entry-text">{selectedLog.results}</h2>
+              }
+            </div>
+            <div onClick={() => {
+              setEditEntry({
+                ...editEntry, 
+                yieldShowInput: true, 
+                changesMade: true,
+              })
+            }}>
+              <h2 className="view-entry-text">Yield: </h2>
+              {editEntry.yieldShowInput ?
+                <input value={selectedLog.yield}></input>
+                :
+                <h2 className="view-entry-text">{selectedLog.yield}</h2>
+              }
+            </div>
+            { editEntry.changesMade && 
+              <div id="view-entry-div-buttons">
+                <Button color="primary" variant="contained" >Save Changes</Button>
+                <Button color="secondary" variant="contained" >Discard Changes</Button>
+              </div>
+            }
+          </form>
         </div>
       </div>
     </div>
