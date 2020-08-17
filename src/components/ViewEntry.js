b8a5cce01f7822a7c050f934827dcba36ea47ffc
ajewe <ajewe@utexas.edu>
2020-08-17 17:49:50 -0500

    updated text color in left nav menu popup

--- src/components/ViewEntry.js
+++ src/components/ViewEntry.js
@@ -15,13 +15,7 @@ export const ViewEntry = props => {
   const userToken = user.token
   const [ sketcher, setSketcher ] = React.useState(null);
   const selectedLog = useSelector( state => state.selectedLog.log )
-  const isLoading = useSelector( state => state.selectedLog.isLoading )
   
-  // const updateLoading = () => {
-  //   if (selectedLog) {
-  //     setIsLoading(false)
-  //   }
-  // }
   const [ editableLog, setEditableLog ] = React.useState({...selectedLog})
 
   const [ editEntry, setEditEntry ] = React.useState({
@@ -39,7 +33,6 @@ export const ViewEntry = props => {
   }, [])
 
   useEffect(() => {
-    // updateLoading()
     setEditEntry({
       ...editEntry,
       procedures: selectedLog?.procedures?.map(() => ({
@@ -126,7 +119,7 @@ export const ViewEntry = props => {
   const handleSubmit = e => {
     e.preventDefault()
     changeDateAndTimeLastUpdated()
-    setSketchData()
+    // setSketchData()
     const payload = { ...editableLog }
     dispatch(changeLog(selectedLogId, payload, userToken))
     history.push("/")
@@ -135,152 +128,152 @@ export const ViewEntry = props => {
   return (
     <>
       <LeftNavigation userToken={ userToken }
-                      user={ user }/>
-        <div id="view-entry-paper">
-          <div id="view-entry-pattern">
-              <div id="view-entry-content">
-                <h1>{selectedLog.book}: Entry {selectedLog.book_entry_number} </h1 >
-                <form onSubmit={ handleSubmit }>
-                  <div onClick={() => {
-                    setEditEntry({
-                      ...editEntry, 
-                      quickInfoShowInput: true, 
-                      changesMade: true,
-                    })
-                  }}>
-                    <h2 className="view-entry-text">Quick Info: </h2>
-                    {editEntry.quickInfoShowInput ? 
-                      <input 
-                        value={editableLog.quick_info} 
-                        name="quick_info"
-                        onChange={ e => handleInputChange(e) }
-                      />
-                      :
-                      <h2 className="view-entry-text">{selectedLog.quick_info} </h2>
-                    }
-                  </div>
-                  <canvas id="canvas-id" onMouseDown={() => canvasClicked()} />
-                  <br />
-                  <h2 className="view-entry-text">Procedure: </h2>
-                    {editableLog.procedures.map((p, i) => {
-                      return (
-                        <div className="view-entry-procedure-div">
-                          {editEntry.procedures[i] && editEntry.procedures[i].dateShowInput ?
-                            <input 
-                              value={p.date} 
-                              name="date"
-                              onChange={ e => handleProcedureChange(e, i) }
-                            />
-                          :
-                            <h2 className="view-entry-procedure-date" 
-                                onClick={() => {
-                                  setEditEntry({
-                                    ...editEntry,
-                                    changesMade: true,
-                                    procedures: [
-                                      ...editEntry.procedures.slice(0, i),
-                                      {
-                                        ...editEntry.procedures[i],
-                                        dateShowInput: true,
-                                      },
-                                      ...editEntry.procedures.slice(i + 1)
-                                    ]
-                                  })
-                                }}
-                            >
-                              {p.date} 
-                            </h2>
-                          }
-                          {editEntry.procedures[i] && editEntry.procedures[i].entryShowInput ?
-                            <input 
-                              value={p.entry} 
-                              name="entry"
-                              onChange={ e => handleProcedureChange(e, i) }
-                            />
-                          :
-                            <h2 className="view-entry-procedure-entry"
-                                onClick={() => {
-                                  setEditEntry({
-                                    ...editEntry,
-                                    changesMade: true,
-                                    procedures: [
-                                      ...editEntry.procedures.slice(0, i),
-                                      {
-                                        ...editEntry.procedures[i],
-                                        entryShowInput: true,
-                                      },
-                                      ...editEntry.procedures.slice(i + 1)
-                                    ]
-                                  })
-                                }}
-                            >
-                              {p.entry}
-                            </h2>
-                          }
-                        </div>
-                      )
-                    })}
-                  <div onClick={() => {
-                    setEditEntry({
-                      ...editEntry, 
-                      resultsShowInput: true, 
-                      changesMade: true,
-                    })
-                  }}>
-                    <h2 className="view-entry-text">Results: </h2>
-                    {editEntry.resultsShowInput ?
-                      <input 
-                        value={editableLog.results}
-                        name="results"
-                        onChange={ e => handleInputChange(e) }
-                      />
-                      :
-                      <h2 className="view-entry-text">{selectedLog.results}</h2>
-                    }
-                  </div>
-                  <div onClick={() => {
-                    setEditEntry({
-                      ...editEntry, 
-                      yieldShowInput: true, 
-                      changesMade: true,
-                    })
-                  }}>
-                    <h2 className="view-entry-text">Yield: </h2>
-                    {editEntry.yieldShowInput ?
-                      <input 
-                        value={editableLog.yield} 
-                        name="yield"
-                        onChange={ e => handleInputChange(e) }
-                      />
-                      :
-                      <h2 className="view-entry-text">{selectedLog.yield}</h2>
-                    }
-                  </div>
-                  {/* Need to update lastUpdated when click 'Save Changes' */}
-                  { editEntry.changesMade && 
-                    <div id="view-entry-div-buttons">
-                      <Button color="primary" 
-                              variant="contained" 
-                              type='submit'
-                      >
-                        Save Changes
-                      </Button>
-                      <Button color="secondary" 
-                              variant="contained" 
-                              onClick={() => {
-                                if (window.confirm('Are you sure you want to discard changes?')) {
-                                  history.push("/")
-                                }
-                              }}
-                      >
-                        Discard Changes
-                      </Button>
-                    </div>
+                      user={ user }/>      
+      <div id="view-entry-paper">
+        <div id="view-entry-pattern">
+            <div id="view-entry-content">
+              <h1>{selectedLog.book}: Entry {selectedLog.book_entry_number} </h1 >
+              <form onSubmit={ handleSubmit }>
+                <div onClick={() => {
+                  setEditEntry({
+                    ...editEntry, 
+                    quickInfoShowInput: true, 
+                    changesMade: true,
+                  })
+                }}>
+                  <h2 className="view-entry-text">Quick Info: </h2>
+                  {editEntry.quickInfoShowInput ? 
+                    <input 
+                      value={editableLog.quick_info} 
+                      name="quick_info"
+                      onChange={ e => handleInputChange(e) }
+                    />
+                    :
+                    <h2 className="view-entry-text">{selectedLog.quick_info} </h2>
                   }
-                </form>
-              </div>
-          </div>
+                </div>
+                <canvas id="canvas-id" onMouseDown={() => canvasClicked()} />
+                <br />
+                <h2 className="view-entry-text">Procedure: </h2>
+                  {editableLog.procedures.map((p, i) => {
+                    return (
+                      <div className="view-entry-procedure-div">
+                        {editEntry.procedures[i] && editEntry.procedures[i].dateShowInput ?
+                          <input 
+                            value={p.date} 
+                            name="date"
+                            onChange={ e => handleProcedureChange(e, i) }
+                          />
+                        :
+                          <h2 className="view-entry-procedure-date" 
+                              onClick={() => {
+                                setEditEntry({
+                                  ...editEntry,
+                                  changesMade: true,
+                                  procedures: [
+                                    ...editEntry.procedures.slice(0, i),
+                                    {
+                                      ...editEntry.procedures[i],
+                                      dateShowInput: true,
+                                    },
+                                    ...editEntry.procedures.slice(i + 1)
+                                  ]
+                                })
+                              }}
+                          >
+                            {p.date} 
+                          </h2>
+                        }
+                        {editEntry.procedures[i] && editEntry.procedures[i].entryShowInput ?
+                          <input 
+                            value={p.entry} 
+                            name="entry"
+                            onChange={ e => handleProcedureChange(e, i) }
+                          />
+                        :
+                          <h2 className="view-entry-procedure-entry"
+                              onClick={() => {
+                                setEditEntry({
+                                  ...editEntry,
+                                  changesMade: true,
+                                  procedures: [
+                                    ...editEntry.procedures.slice(0, i),
+                                    {
+                                      ...editEntry.procedures[i],
+                                      entryShowInput: true,
+                                    },
+                                    ...editEntry.procedures.slice(i + 1)
+                                  ]
+                                })
+                              }}
+                          >
+                            {p.entry}
+                          </h2>
+                        }
+                      </div>
+                    )
+                  })}
+                <div onClick={() => {
+                  setEditEntry({
+                    ...editEntry, 
+                    resultsShowInput: true, 
+                    changesMade: true,
+                  })
+                }}>
+                  <h2 className="view-entry-text">Results: </h2>
+                  {editEntry.resultsShowInput ?
+                    <input 
+                      value={editableLog.results}
+                      name="results"
+                      onChange={ e => handleInputChange(e) }
+                    />
+                    :
+                    <h2 className="view-entry-text">{selectedLog.results}</h2>
+                  }
+                </div>
+                <div onClick={() => {
+                  setEditEntry({
+                    ...editEntry, 
+                    yieldShowInput: true, 
+                    changesMade: true,
+                  })
+                }}>
+                  <h2 className="view-entry-text">Yield: </h2>
+                  {editEntry.yieldShowInput ?
+                    <input 
+                      value={editableLog.yield} 
+                      name="yield"
+                      onChange={ e => handleInputChange(e) }
+                    />
+                    :
+                    <h2 className="view-entry-text">{selectedLog.yield}</h2>
+                  }
+                </div>
+                { editEntry.changesMade && 
+                  <div id="view-entry-div-buttons">
+                    <Button color="primary" 
+                            variant="contained" 
+                            type='submit'
+                    >
+                      Save Changes
+                    </Button>
+                    <Button color="secondary" 
+                            variant="contained" 
+                            onClick={() => {
+                              if (window.confirm('Are you sure you want to discard changes?')) {
+                                history.push("/")
+                              }
+                            }}
+                    >
+                      Discard Changes
+                    </Button>
+                  </div>
+                }
+              </form>
+            </div>
         </div>
+      </div>
+
     </>
   )
 }
--- src/components/navigation/MenuPopUp.js
+++ src/components/navigation/MenuPopUp.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
-import { makeStyles, Popper, Grow, Paper, ClickAwayListener, MenuList, MenuItem } from '@material-ui/core'
+import { Popper, Grow, Paper, ClickAwayListener, MenuList, MenuItem } from '@material-ui/core'
 
-const useStyles = makeStyles(() => ({
-  listItem: {
-    color: "rgb(32, 65, 97)",
-  }
-}))
 
 export const MenuPopUp = (props) => {
-  const classes = useStyles();
-
   return (
     <Popper open={props.open} 
             anchorEl={props.anchorEl} 
@@ -30,12 +23,11 @@ export const MenuPopUp = (props) => {
                         <Link to={ menuItem.linkTo + props.extraLinkAttribute }
                               onClick={ menuItem.handleClick }
                               className="link" >
-                         <MenuItem className={ classes.listItem }>
+                         <MenuItem>
                            { menuItem.text }
                         </MenuItem>
                       </Link> :
-                      <MenuItem onClick={ menuItem.handleClick } 
-                                className={ classes.listItem }>
+                      <MenuItem onClick={ menuItem.handleClick }>
                           { menuItem.text }
                       </MenuItem> 
                       }
