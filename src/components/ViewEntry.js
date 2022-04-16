9f958d24228ac4f5b3960371fceabc723c1ab0ea
ajewe <ajewe@utexas.edu>
2022-04-16 17:49:53 -0500

    fixing styles, restyle view entry page

--- src/components/ViewEntry.js
+++ src/components/ViewEntry.js
@@ -43,7 +43,7 @@ export const ViewEntry = (props) => {
     setEditableLog({ ...selectedLog });
 
     if (!sketcher) {
-      //make responsive
+      // todo make responsive
       let myCanvas = new ChemDoodle.SketcherCanvas("canvas-id", "600", "350", {
         useServices: false,
         oneMolecule: false,
@@ -127,49 +127,49 @@ export const ViewEntry = (props) => {
 
   return (
     <LeftNavigationContainer userToken={userToken} user={user}>
-      <div id="view-entry-paper">
-        <div id="view-entry-pattern">
-          <div id="view-entry-content">
-            <h1>
-              {selectedLog.book}: Entry {selectedLog.book_entry_number}{" "}
-            </h1>
-            <form onSubmit={handleSubmit}>
-              <div
-                onClick={() => {
-                  setEditEntry({
-                    ...editEntry,
-                    quickInfoShowInput: true,
-                    changesMade: true,
-                  });
-                }}
-              >
-                <h2 className="view-entry-text">Quick Info: </h2>
-                {editEntry.quickInfoShowInput ? (
-                  <input
-                    value={editableLog.quick_info}
-                    name="quick_info"
-                    onChange={(e) => handleInputChange(e)}
-                  />
-                ) : (
-                  <h2 className="view-entry-text">{selectedLog.quick_info} </h2>
-                )}
-              </div>
-              <canvas id="canvas-id" onMouseDown={() => canvasClicked()} />
-              <br />
-              <h2 className="view-entry-text">Procedure: </h2>
+      <div className="h-full w-full bg-paper-pattern">
+        <div className="p-4 pt-2 pl-[16%]">
+          <h1 className="text-3xl tracking-wide my-2">
+            {selectedLog.book}: Entry {selectedLog.book_entry_number}{" "}
+          </h1>
+          <form onSubmit={handleSubmit}>
+            <div
+              className="mb-2 text-2xl tracking-wide"
+              onClick={() => {
+                setEditEntry({
+                  ...editEntry,
+                  quickInfoShowInput: true,
+                  changesMade: true,
+                });
+              }}
+            >
+              <h2 className="inline">Quick Info: </h2>
+              {editEntry.quickInfoShowInput ? (
+                <input
+                  className="w-full border border-gray-300"
+                  value={editableLog.quick_info}
+                  name="quick_info"
+                  onChange={(e) => handleInputChange(e)}
+                />
+              ) : (
+                <h2 className="inline">{selectedLog.quick_info} </h2>
+              )}
+            </div>
+            <canvas id="canvas-id" onMouseDown={() => canvasClicked()} />
+            <h3 className="my-2 text-xl tracking-wide">Procedure: </h3>
+            <ul>
               {editableLog.procedures.map((p, i) => {
                 return (
-                  <div className="view-entry-procedure-div">
-                    {editEntry.procedures[i] &&
-                    editEntry.procedures[i].dateShowInput ? (
+                  <li className="my-1">
+                    {editEntry.procedures[i]?.dateShowInput ? (
                       <input
+                        className="w-full border border-gray-300"
                         value={p.date}
                         name="date"
                         onChange={(e) => handleProcedureChange(e, i)}
                       />
                     ) : (
-                      <h2
-                        className="view-entry-procedure-date"
+                      <p
                         onClick={() => {
                           setEditEntry({
                             ...editEntry,
@@ -186,18 +186,18 @@ export const ViewEntry = (props) => {
                         }}
                       >
                         {p.date}
-                      </h2>
+                      </p>
                     )}
-                    {editEntry.procedures[i] &&
-                    editEntry.procedures[i].entryShowInput ? (
+                    {editEntry.procedures[i]?.entryShowInput ? (
                       <input
+                        className="w-full border border-gray-300"
                         value={p.entry}
                         name="entry"
                         onChange={(e) => handleProcedureChange(e, i)}
                       />
                     ) : (
                       <h2
-                        className="view-entry-procedure-entry"
+                        className="ml-4"
                         onClick={() => {
                           setEditEntry({
                             ...editEntry,
@@ -216,73 +216,77 @@ export const ViewEntry = (props) => {
                         {p.entry}
                       </h2>
                     )}
-                  </div>
+                  </li>
                 );
               })}
-              <div
-                onClick={() => {
-                  setEditEntry({
-                    ...editEntry,
-                    resultsShowInput: true,
-                    changesMade: true,
-                  });
-                }}
-              >
-                <h2 className="view-entry-text">Results: </h2>
-                {editEntry.resultsShowInput ? (
-                  <input
-                    value={editableLog.results}
-                    name="results"
-                    onChange={(e) => handleInputChange(e)}
-                  />
-                ) : (
-                  <h2 className="view-entry-text">{selectedLog.results}</h2>
-                )}
-              </div>
-              <div
-                onClick={() => {
-                  setEditEntry({
-                    ...editEntry,
-                    yieldShowInput: true,
-                    changesMade: true,
-                  });
-                }}
-              >
-                <h2 className="view-entry-text">Yield: </h2>
-                {editEntry.yieldShowInput ? (
-                  <input
-                    value={editableLog.yield}
-                    name="yield"
-                    onChange={(e) => handleInputChange(e)}
-                  />
-                ) : (
-                  <h2 className="view-entry-text">{selectedLog.yield}</h2>
-                )}
-              </div>
-              {editEntry.changesMade && (
-                <div id="view-entry-div-buttons">
-                  {/* <Button color="primary" variant="contained" type="submit"> */}
-                  Save Changes
-                  {/* </Button> */}
-                  {/* <Button
-                    color="secondary"
-                    variant="contained"
-                    onClick={() => {
-                      if (
-                        window.confirm(
-                          "Are you sure you want to discard changes?"
-                        )
-                      ) {
-                        history.push("/");
-                      }
-                    }}
-                  > */}
-                  Discard Changes
-                  {/* </Button> */}
-                </div>
+            </ul>
+            <div
+              onClick={() => {
+                setEditEntry({
+                  ...editEntry,
+                  resultsShowInput: true,
+                  changesMade: true,
+                });
+              }}
+            >
+              <h3 className="my-2 text-xl tracking-wide">Results: </h3>
+              {editEntry.resultsShowInput ? (
+                <input
+                  className="w-full border border-gray-300"
+                  value={editableLog.results}
+                  name="results"
+                  onChange={(e) => handleInputChange(e)}
+                />
+              ) : (
+                <h3 className="ml-4">{selectedLog.results}</h3>
               )}
-            </form>
-          </div>
+            </div>
+            <div
+              onClick={() => {
+                setEditEntry({
+                  ...editEntry,
+                  yieldShowInput: true,
+                  changesMade: true,
+                });
+              }}
+            >
+              <h3 className="my-2 text-xl tracking-wide">Yield: </h3>
+              {editEntry.yieldShowInput ? (
+                <input
+                  className="border border-gray-300"
+                  value={editableLog.yield}
+                  name="yield"
+                  onChange={(e) => handleInputChange(e)}
+                />
+              ) : (
+                <h2 className="ml-4">{selectedLog.yield}</h2>
+              )}
+            </div>
+            {editEntry.changesMade ? (
+              <div className="w-full flex justify-center mt-6">
+                <button
+                  className="w-4/12 py-2 mr-2 border border-solid bg-indigo-500 text-white rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  type="submit"
+                >
+                  Save Changes
+                </button>
+                <button
+                  className="w-4/12 py-2 border border-solid bg-red-500 text-white rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                  onClick={() => {
+                    if (
+                      window.confirm(
+                        "Are you sure you want to discard changes?"
+                      )
+                    ) {
+                      history.push("/");
+                    }
+                  }}
+                >
+                  Discard Changes
+                </button>
+              </div>
+            ) : null}
+          </form>
         </div>
       </div>
     </LeftNavigationContainer>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "paper-pattern":
+          "repeating-linear-gradient(white 0px, white 24px, teal 25px)",
+      },
+      width: {
+        half: "2px",
+      },
+    },
   },
   plugins: [require("@tailwindcss/forms")],
 };
